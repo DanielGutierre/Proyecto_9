ab8d26eca7bfba57dc58e8cbd6caf7800b8cb004
DanielGutierre <earody4@gmail.com>
2022-05-30 18:35:02 -0600

    mi primer commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, View, ScrollView } from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+} 
+
+
+export default function App() {
+  return (
+   <ScrollView>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} />
+
+      
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
